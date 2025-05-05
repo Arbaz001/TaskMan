2fb419a051d3cc7b2829e7d11074727b07ce139a
Arbaz001 <arbaza3212btechcse2025@kccitm.edu.in>
2025-05-05 10:16:28 +0530

    Fix issues for default dashoboard to login

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import Sidebar from './components/Sidebar'
-import { Routes, Route, useLocation } from 'react-router-dom'
+import { Routes, Route, useLocation, Navigate } from 'react-router-dom'
 import Dashboard from './pages/Dashboard'
 import Login from './pages/Login'
 import Signup from './pages/Signup'
@@ -11,6 +11,11 @@ import Profile from './pages/Profile'
 import TaskForm from './pages/TaskForm'
 import { ThemeProvider, useTheme } from './components/ThemeContext';
 import DarkModeToggle from './components/DarkModeToggle';
+
+const PrivateRoute = ({ children }) => {
+  const token = localStorage.getItem('token');
+  return token ? children : <Navigate to="/login" replace />;
+};
 
 const AppContent = () => {
   const location = useLocation();
@@ -23,13 +28,13 @@ const AppContent = () => {
         {!hideSidebar && <Sidebar />}
         <main className="flex-1 p-6 min-h-screen">
           <Routes>
-            <Route path="/" element={<Dashboard />} />
             <Route path="/login" element={<Login />} />
             <Route path="/signup" element={<Signup />} />
-            <Route path="/pending" element={<PendingTasks />} />
-            <Route path="/complete" element={<CompletedTasks />} />
-            <Route path="/profile" element={<Profile />} />
-            <Route path="/taskform" element={<TaskForm />} />
+            <Route path="/" element={<PrivateRoute><Dashboard /></PrivateRoute>} />
+            <Route path="/pending" element={<PrivateRoute><PendingTasks /></PrivateRoute>} />
+            <Route path="/complete" element={<PrivateRoute><CompletedTasks /></PrivateRoute>} />
+            <Route path="/profile" element={<PrivateRoute><Profile /></PrivateRoute>} />
+            <Route path="/taskform" element={<PrivateRoute><TaskForm /></PrivateRoute>} />
           </Routes>
         </main>
         {!hideSidebar && <DarkModeToggle />}
